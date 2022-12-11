e5f68b4d324c6c6f8802320de16ddff0ebcde03b
selinugurlus <selinugurlu2002@hotmail.com>
2022-12-11 14:15:53 +0300

    katmanlar

---
+++ EntityLayer/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class User
+    {
+        [Key]
+        public int user_id { get; set; }
+        [StringLength(50)]
+        public string user_name { get; set; }
+        [StringLength(50)]
+        public string user_surname { get; set; }
+        [StringLength(50)]
+        public string user_mail { get; set; }
+        [StringLength(50)]
+        public string user_password { get; set; }
+
+    }
+}
