3af82287517abff57affced950a9142e9bd85409
selinugurlus <selinugurlu2002@hotmail.com>
2022-12-11 23:36:32 +0300

    mssql bağlantısı

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -5,6 +5,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.EntityFramework;
 
 namespace DataAccessLayer.Concrete
 {
